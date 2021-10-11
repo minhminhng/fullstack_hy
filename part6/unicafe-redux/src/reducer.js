8b48379f34362890c5c36214141f60e67571fcd5
minhnguyen93 <minhnguyen93@outlook.com>
2021-10-11 20:02:28 +0300

    anedote vote

---
+++ part6/unicafe-redux/src/reducer.js
@@ -0,0 +1,40 @@
+const initialState = {
+  good: 0,
+  ok: 0,
+  bad: 0
+}
+
+const counterReducer = (state = initialState, action) => {
+  console.log(action)
+  switch (action.type) {
+    case 'GOOD':{
+      const changedState = {
+        ...state, good: state.good + 1
+      }
+      return changedState
+    }
+      
+    case 'OK':{
+      const changedState = {
+        ...state, ok: state.ok + 1
+      }
+      return changedState
+    }
+    case 'BAD':{
+      const changedState = {
+        ...state, bad: state.bad + 1
+      }
+      return changedState
+    }
+    case 'ZERO':{
+      const changedState = {
+        ...initialState
+      }
+      return changedState
+    }
+    default: return state
+  }
+  
+}
+
+export default counterReducer

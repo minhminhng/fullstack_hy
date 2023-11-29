ee2b6013183d49d185aba55689990be5ac66ca11
Minh Nguyen <minhnguyen93@outlook.com>
2023-11-29 16:41:40 +0200

    Anecdote with ReactQuery

--- part6/query-anecdotes/src/App.jsx
+++ part6/query-anecdotes/src/App.jsx
@@ -1,26 +1,58 @@
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { getAnecdotes, createAnecdote, updateAnecdote } from './request'
+
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 
 const App = () => {
+  const queryClient = useQueryClient()
+  const newAnecdoteMutation = useMutation({ 
+    mutationFn: createAnecdote,
+    onSuccess: (newAnecdote) => {
+      const anecdotes = queryClient.getQueryData([anecdotes])
+      queryClient.setQueryData(['anecdotes'], anecdotes.concat(newAnecdote))
+    }
+  })
 
+  const updateAnecdoteMutation = useMutation({ 
+    mutationFn: updateAnecdote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['anecdotes']})
+    }
+  })
+
+  const addAnecdote = async (event) => {
+    event.preventDefault()
+    const content = event.target.anecdote.value
+    event.target.anecdote.value = ''
+    newAnecdoteMutation.mutate({ content, votes: 0 })    
+  }
   const handleVote = (anecdote) => {
-    console.log('vote')
+    updateAnecdoteMutation.mutate({...anecdote, votes: anecdote.votes + 1 })
   }
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: getAnecdotes,
+    refetchOnWindowFocus: false
+  })
+
+  if ( result.isLoading ) {
+    return <div>anecdote service not available due to problems in server</div>
+  }
+
+  if ( result.isError ) {
+    return <div>Error: {result.error.message}</div>
+  }
+
+  const anecdotes = result.data
 
   return (
     <div>
       <h3>Anecdote app</h3>
     
       <Notification />
-      <AnecdoteForm />
+      <AnecdoteForm handleCreate={addAnecdote}/>
     
       {anecdotes.map(anecdote =>
         <div key={anecdote.id}>
--- part6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,16 +1,16 @@
-const AnecdoteForm = () => {
+const AnecdoteForm = ({ handleCreate }) => {
 
-  const onCreate = (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    console.log('new anecdote')
-}
+  // const onCreate = (event) => {
+  //   event.preventDefault()
+  //   const content = event.target.anecdote.value
+  //   event.target.anecdote.value = ''
+  //   console.log('new anecdote')
+  // }
 
   return (
     <div>
       <h3>create new</h3>
-      <form onSubmit={onCreate}>
+      <form onSubmit={handleCreate}>
         <input name='anecdote' />
         <button type="submit">create</button>
       </form>

182836d905b1a187fb035eaa6855953923b2879e
Minh Nguyen <minhnguyen93@outlook.com>
2023-12-04 09:23:00 +0200

    Anecdotes and hooks (exercise 7.4 - 7.6)

--- part7/routed-anecdotes/components/CreateNew.jsx
+++ part7/routed-anecdotes/components/CreateNew.jsx
@@ -1,22 +1,27 @@
-import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { useField } from '../hooks'
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content = useField('content')
+  const author = useField('author')
+  const info = useField('info')
   const navigate = useNavigate()
-
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })    
     navigate('/')
+  }
+
+  const clearForm = () => {
+    content.onReset()
+    author.onReset()
+    info.onReset()
   }
 
   return (
@@ -25,18 +30,20 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input {...content} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input {...info} />
         </div>
         <button>create</button>
+        <button type="button" onClick={clearForm}>reset</button>
       </form>
+      
     </div>
   )
 
---
+++ part7/routed-anecdotes/hooks/index.js
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+export const useField = (name) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  const onReset = () => {
+    setValue('')
+  }
+
+  return {
+    name,
+    value,
+    onChange,
+    onReset
+  }
+}
+
+export const useAnotherHook = () => {
+
+}

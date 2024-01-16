8d38adb92b0c5d3e9d0f1cec7166bfaee2b76f3a
Minh Nguyen <minhnguyen93@outlook.com>
2024-01-16 13:46:22 +0200

    Preparation exercises 9.8 - 9.9

--- part9/patient-backend/index.ts
+++ part9/patient-backend/index.ts
@@ -1,12 +1,20 @@
 import express from 'express';
+import cors from 'cors';
 const app = express();
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = 3001;
 
-app.get('/ping', (_req, res) => {
+app.use(cors());
+
+app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
+});
+
+app.get('/api/patients', (_req, res) => {
+  console.log('checking patients');
+  res.send('request received');
 });
 
 app.listen(PORT, () => {
